6f9c12444ad139f5b1845bb2684357801f5f42a0
root <root@gustavohenrique-Inspiron-5421.(none)>
2014-03-01 22:12:02 -0300

    /home/gustavo-henrique/NetBeansProjects/Pizzaria

---
+++ src/persistencia/DadosFuncionario.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+import modelo.Funcionario;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gustavo-henrique
+ */
+public class DadosFuncionario {
+    private ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+    
+}
