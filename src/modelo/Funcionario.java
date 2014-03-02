98273654b94a87594a6360e37f19e2bc07f5b865
root <root@gustavohenrique-Inspiron-5421.(none)>
2014-03-02 14:57:50 -0300

    /home/gustavo-henrique/NetBeansProjects

--- src/modelo/Funcionario.java
+++ src/modelo/Funcionario.java
@@ -8,8 +8,51 @@ package modelo;
  *
  * @author gustavo-henrique
  */
-public class Funcionario {
-    private String nome;
-    private String cpf;
-    
+public class Funcionario extends Pessoa {
+    private String rg;
+    private int idade;
+    private String usuario;
+    private String senha;
+    private double salario;
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+         
 }
