7586fbb7d9d058721a6b12ccf2394ed699fc65b4
root <root@gustavohenrique-Inspiron-5421.(none)>
2014-03-02 14:50:05 -0300

    /home/gustavo-henrique/NetBeansProjects/Pizzaria/

---
+++ src/modelo/Pessoa.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author gustavo-henrique
+ */
+public class Pessoa {
+        private String nome;
+        private String sobrenome;
+        private String cpf;
+        private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
+}
